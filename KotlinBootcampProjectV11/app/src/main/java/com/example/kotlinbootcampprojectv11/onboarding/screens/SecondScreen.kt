642cc33d1cc1d5d051d98999fd0f42bf82a480d4
kadirartan <61347351+kadirartan@users.noreply.github.com>
2021-05-08 10:23:00 +0300

    Bootcamp projemi ilk kez yükledim
    Kotlin Bootcamp projesi

---
+++ KotlinBootcampProjectV11/app/src/main/java/com/example/kotlinbootcampprojectv11/onboarding/screens/SecondScreen.kt
@@ -0,0 +1,35 @@
+package com.example.kotlinbootcampprojectv11.onboarding.screens
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.kotlinbootcampprojectv11.R
+
+class SecondScreen : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        val view =  inflater.inflate(R.layout.fragment_second_screen, container, false)
+
+        view.finish.seOnClickListener {
+            findNavController().navigate(R.id.action_viewPagerFragment_to_beginFragment)
+        }
+
+        return view
+    }
+
+    private fun onBoardingFinished(){
+        val sharedPref =requireActivity().getSharedPreferences("onBoarding", Context.MODE_PRIVATE)
+        val editor = sharedPref.edit()
+        editor.putBoolean("Finished", true)
+        editor.apply()
+    }
+}
